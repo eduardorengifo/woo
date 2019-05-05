35724b6486fc818156f3439896f3e299b5622e5b
Eduardo Rengifo <eduardorengifo5@gmail.com>
2019-05-05 16:42:58 -0500

    Adding Link component in basics directory

---
+++ components/basics/Link/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import Box from '../Box';
+
+const themed = key => props => props.theme[key];
+
+const Link = styled(Box)(themed('Link'));
+
+Link.defaultProps = {
+  as: 'a',
+  color: 'blue'
+};
+
+export default Link;
