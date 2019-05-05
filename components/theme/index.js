e853e62672adb5cfc1041e674908ee8395e7ed63
Eduardo Rengifo <eduardorengifo5@gmail.com>
2019-05-05 12:35:34 -0500

    Implement addons in storybook and theme config

---
+++ components/theme/index.js
@@ -0,0 +1,85 @@
+/* eslint-disable prefer-destructuring */
+export { default as GlobalStyle } from './GlobalStyle';
+
+// Colors
+
+const baseColors = {
+  black: '#000',
+  white: '#fff'
+};
+
+const colors = {
+  text: baseColors.black,
+  background: baseColors.white,
+  primary: '#00f',
+  secondary: '#00a',
+  gray: '#eee',
+  lightgray: '#fafafa'
+};
+
+// Breakpoints
+const breakpoints = ['40em', '52em', '64em', '80em'];
+
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+// Font Sizes
+const fontSizes = [12, 14, 16, 20, 24, 32, 48, 64, 72];
+
+fontSizes.body = fontSizes[2];
+fontSizes.display = fontSizes[5];
+
+// Space
+const space = [0, 4, 8, 16, 32, 64, 128, 256, 512];
+
+export default {
+  breakpoints,
+  fontSizes,
+  colors,
+  space,
+  fonts: {
+    sans: 'system-ui, sans-serif',
+    mono: 'Menlo, monospace'
+  },
+  mediaQueries: {
+    sm: `@media screen and (min-width: ${breakpoints[0]})`,
+    md: `@media screen and (min-width: ${breakpoints[1]})`,
+    lg: `@media screen and (min-width: ${breakpoints[2]})`,
+    xl: `@media screen and (min-width: ${breakpoints[3]})`
+  },
+  lineHeights: {
+    body: 1.75,
+    heading: 1.25
+  },
+  shadows: {
+    sm: '0 0 4px rgba(0, 0, 0, .125)',
+    lg: '0 0 24px rgba(0, 0, 0, .125)'
+  },
+  colorStyles: {
+    warning: {
+      color: 'black',
+      backgroundColor: 'orange'
+    },
+    error: {
+      color: 'white',
+      backgroundColor: 'red'
+    }
+  },
+  buttons: {
+    primary: {
+      color: baseColors.white,
+      backgroundColor: colors.primary
+    },
+    secondary: {
+      color: baseColors.white,
+      backgroundColor: colors.secondary
+    },
+    outline: {
+      color: colors.primary,
+      backgroundColor: 'transparent',
+      boxShadow: 'inset 0 0 0 2px'
+    }
+  }
+};
