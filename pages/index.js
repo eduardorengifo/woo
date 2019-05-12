64d55f723a66117b6b18962d9a44f9b23f2da3d3
Eduardo Rengifo <eduardorengifo5@gmail.com>
2019-05-12 12:45:55 -0500

    Implement ProductCard in home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
 import PanelLayout from '../layouts/Panel';
+import { Flex, Box } from '../components/basics';
+import ProductCard from '../components/ui/Product/Card';
 
-export default () => <PanelLayout>My page</PanelLayout>;
+const numbers = Array.from(Array(48).keys());
+
+export default () => (
+  <PanelLayout>
+    <Flex flexWrap="wrap" px={-2} my={3}>
+      {numbers.map(number => (
+        <Box key={number} width={[1 / 2, 1 / 4, 1 / 6, 1 / 8]} mb={3} px={2}>
+          <ProductCard
+            product={{
+              name: `Product ${number}`,
+              price: 'S/ 400',
+              priceDiscount: 'S/ 200',
+              image: 'https://source.unsplash.com/random/225x150'
+            }}
+          />
+        </Box>
+      ))}
+    </Flex>
+  </PanelLayout>
+);
