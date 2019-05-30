3fe687c45a6cde37d11ed4b11c42ea762ea93995
Eduardo Rengifo <eduardorengifo5@gmail.com>
2019-05-29 22:10:46 -0500

    Implement ua support

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,13 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import theme, { GlobalStyle } from '../components/theme';
+import { UserAgentProvider } from '../lib/ua';
 
 export default class WooApp extends App {
+  state = {
+    userAgent: this.props.userAgent
+  };
+
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
 
@@ -12,17 +17,39 @@ export default class WooApp extends App {
       pageProps = await Component.getInitialProps(ctx);
     }
 
-    return { pageProps };
+    const userAgent = ctx.req
+      ? ctx.req.headers['user-agent']
+      : navigator.userAgent;
+
+    return { pageProps, userAgent };
   }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = () => {
+    const { userAgent } = this.state;
+    if (userAgent !== navigator.userAgent) {
+      this.setState({ userAgent: navigator.userAgent });
+    }
+  };
 
   render() {
     const { Component, pageProps } = this.props;
+    const { userAgent } = this.state;
     return (
       <Container>
         <ThemeProvider theme={theme}>
           <>
             <GlobalStyle />
-            <Component {...pageProps} />
+            <UserAgentProvider value={userAgent}>
+              <Component {...pageProps} />
+            </UserAgentProvider>
           </>
         </ThemeProvider>
       </Container>
